80908afa4394d4c89ded940fc3679f0d77e87ced
Spencer Morris <spencermorris22@gmail.com>
2015-09-25 11:29:26 -0500

    Have connection working. Just need to handle different parts now of the connection.

--- app/src/main/java/com/willydevelopment/jj/sphero/HomeScreen.java
+++ app/src/main/java/com/willydevelopment/jj/sphero/HomeScreen.java
@@ -5,23 +5,73 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.orbotix.ConvenienceRobot;
 import com.orbotix.DualStackDiscoveryAgent;
+import com.orbotix.Ollie;
+import com.orbotix.Sphero;
+import com.orbotix.classic.RobotClassic;
+import com.orbotix.command.RGBLEDOutputCommand;
+import com.orbotix.command.RollCommand;
+import com.orbotix.common.DiscoveryException;
+import com.orbotix.common.Robot;
 import com.orbotix.common.RobotChangedStateListener;
+import com.orbotix.le.RobotLE;
 
 public class HomeScreen extends AppCompatActivity {
+
+    private ConvenienceRobot mRobot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        DualStackDiscoveryAgent.getInstance().addRobotStateListener(new RobotChangedStateListener() {
+            @Override
+            public void handleRobotChangedState(Robot robot, RobotChangedStateNotificationType type) {
+                switch (type) {
+                    case Online:
+                        if (robot instanceof RobotClassic) {
+                            mRobot = new Sphero(robot);
+                            mRobot.sendCommand( new RGBLEDOutputCommand( 0.5f, 0.5f, 0.5f ) );
+                            mRobot.sendCommand( new RollCommand( 90f, .5f, RollCommand.State.GO ) );
+                        }
+                        if (robot instanceof RobotLE) {
+                            mRobot = new Ollie(robot);
+                        }
+
+                        break;
+                    case Disconnected:
+                        break;
+                }
+            }
+        });
     }
 
-    DualStackDiscoveryAgent.getInstance().addRobotStateListenernew( RobotChangedStateListener() {
-        @Override
-        public void handleRobotChangedState( Robot robot, RobotChangedStateNotificationType type ) {
 
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // This line assumes that this object is a Context
+        try {
+            DualStackDiscoveryAgent.getInstance().startDiscovery(this);
+        } catch( DiscoveryException e ) {
+            //handle exception
         }
-    });
+    }
+
+    @Override
+    protected void onStop() {
+        if( mRobot != null )
+            mRobot.disconnect();
+        super.onStop();
+    }
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
